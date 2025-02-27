08773f07a9082694c0fdca7e6b6b5bab99ba759b
Kreijstal <rainb@tfwno.gf>
2025-02-27 20:15:28 +0000

    we add more things to string.h

--- string.h
+++ string.h
@@ -2,9 +2,18 @@
 #define STRING_H
 
 #include <stddef.h>
+
 char *strcpy(char *dest, const char *src);
 char *strncpy(char *dest, const char *src, size_t n);
 int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
 size_t strlen(const char *s);
+char *strchr(const char *s, int c);
+char *strrchr(const char *s, int c);
+char *strstr(const char *haystack, const char *needle);
+char *strpbrk(const char *s, const char *accept);
+size_t strspn(const char *s, const char *accept);
+size_t strcspn(const char *s, const char *reject);
+char *strtok(char *s, const char *delim);
 
 #endif
