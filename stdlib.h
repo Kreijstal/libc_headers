ad0e83f94d9ef3c7f6610b9ccf349462fab37fa8
Kreijstal <rainb@tfwno.gf>
2025-02-28 17:54:47 +0000

    attempting to make headers more robust

--- stdlib.h
+++ stdlib.h
@@ -1,9 +1,11 @@
 #ifndef STDLIB_H
 #define STDLIB_H
 
-#include <stddef.h>
-void *malloc(size_t size);
-void free(void *ptr);
-void exit(int status);
+typedef unsigned long size_t;
+
+void* malloc(size_t size);
+void* realloc(void* ptr, size_t size);
+void free(void* ptr);
+char* getenv(const char* name);
 
 #endif
--- string.h
+++ string.h
@@ -3,23 +3,6 @@
 
 #include <stddef.h>
 
-char *strcpy(char *dest, const char *src);
-char *strncpy(char *dest, const char *src, size_t n);
-int strcmp(const char *s1, const char *s2);
-int strncmp(const char *s1, const char *s2, size_t n);
-size_t strlen(const char *s);
-char *strchr(const char *s, int c);
-char *strrchr(const char *s, int c);
-char *strstr(const char *haystack, const char *needle);
-char *strpbrk(const char *s, const char *accept);
-size_t strspn(const char *s, const char *accept);
-size_t strcspn(const char *s, const char *reject);
-char *strtok(char *s, const char *delim);
-
-/* Memory and string duplication functions */
-void *memcpy(void *dest, const void *src, size_t n);
-char *strdup(const char *s);
-
-char *stpcpy(char *dest, const char *src);
+void *mempcpy(void *dest, const void *src, size_t n);
 
 #endif
