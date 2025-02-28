a98ef75f175f9330eb045428095ef302b2366884
Kreijstal <rainb@tfwno.gf>
2025-02-28 18:20:44 +0000

    add sys/types.h

---
+++ bits/types.h
@@ -0,0 +1,13 @@
+#ifndef _BITS_TYPES_H
+#define _BITS_TYPES_H
+
+#include <sys/types.h>
+
+/* Basic types */
+typedef int pid_t;
+typedef unsigned int uid_t;
+typedef unsigned int gid_t;
+typedef long time_t;
+typedef long suseconds_t;
+
+#endif /* _BITS_TYPES_H */
