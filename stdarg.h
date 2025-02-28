805c6a1d11cf3dd3735fe0e277dfe5dc358cc68b
Juan Nicolas Pardo Martin <kreijstal@hotmail.com>
2025-02-28 19:25:31 +0100

    Add tcc headers

--- stdarg.h
+++ stdarg.h
@@ -1,9 +1,14 @@
-#ifndef STDARG_H
-#define STDARG_H
+#ifndef _STDARG_H
+#define _STDARG_H
 
 typedef __builtin_va_list va_list;
-#define va_start(v, l) __builtin_va_start(v, l)
-#define va_arg(v, t)   __builtin_va_arg(v, t)
-#define va_end(v)      __builtin_va_end(v)
+#define va_start __builtin_va_start
+#define va_arg __builtin_va_arg
+#define va_copy __builtin_va_copy
+#define va_end __builtin_va_end
 
-#endif
+/* fix a buggy dependency on GCC in libio.h */
+typedef va_list __gnuc_va_list;
+#define _VA_LIST_DEFINED
+
+#endif /* _STDARG_H */
