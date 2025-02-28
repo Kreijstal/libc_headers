125d047314697cebf8cf348ba97b886b20fcc946
Kreijstal <rainb@tfwno.gf>
2025-02-28 16:22:27 +0000

    Adding uinst

--- unistd.h
+++ unistd.h
@@ -6,4 +6,14 @@ int close(int fd);
 ssize_t read(int fd, void *buf, size_t count);
 ssize_t write(int fd, const void *buf, size_t count);
 
+/* Access modes for eaccess() */
+#define F_OK 0 /* Test for existence */
+#define X_OK 1 /* Test for execute permission */
+#define W_OK 2 /* Test for write permission */
+#define R_OK 4 /* Test for read permission */
+
+/* File access functions */
+int access(const char *pathname, int mode);
+int eaccess(const char *pathname, int mode);
+
 #endif
