ae6b06365cc8b7ceea24ca23c52b447ac7288e17
Kreijstal <rainb@tfwno.gf>
2025-03-13 18:49:19 +0000

    time

---
+++ sys/time.h
@@ -0,0 +1,32 @@
+#ifndef _SYS_TIME_H
+#define _SYS_TIME_H
+
+#include <time.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct timeval {
+  time_t      tv_sec;   /* seconds */
+  suseconds_t tv_usec;  /* microseconds */
+};
+
+struct timezone {
+  int tz_minuteswest; /* minutes west of Greenwich */
+  int tz_dsttime;     /* type of DST correction */
+};
+
+int gettimeofday(struct timeval *tv, struct timezone *tz);
+int settimeofday(const struct timeval *tv, const struct timezone *tz);
+
+#define timercmp(a, b, CMP) \
+  (((a)->tv_sec == (b)->tv_sec) ? \
+   ((a)->tv_usec CMP (b)->tv_usec) : \
+   ((a)->tv_sec CMP (b)->tv_sec))
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SYS_TIME_H */
