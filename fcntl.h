e38293a02ac3a59797a1a0794a07fc3b5e846228
Kreijstal <rainb@tfwno.gf>
2025-03-13 16:40:30 +0000

    create fcntl.h

---
+++ fcntl.h
@@ -0,0 +1,28 @@
+#ifndef _FCNTL_H
+#define _FCNTL_H
+
+#include <sys/types.h>
+
+/* File access modes */
+#define O_RDONLY    00
+#define O_WRONLY    01
+#define O_RDWR      02
+
+/* File creation flags */
+#define O_CREAT     0100
+#define O_EXCL      0200
+#define O_TRUNC     01000
+#define O_APPEND    02000
+
+/* File status flags */
+#define O_NONBLOCK  04000
+#define O_NDELAY    O_NONBLOCK
+#define O_SYNC      04010000
+#define O_ASYNC     020000
+
+/* Function declarations */
+int open(const char *pathname, int flags, ...);
+int creat(const char *pathname, mode_t mode);
+int fcntl(int fd, int cmd, ...);
+
+#endif /* _FCNTL_H */
